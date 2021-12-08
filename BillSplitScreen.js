d8a024505dcd38df587310c060b8d51492ff51a5
Yixin Zheng <yixinzh@umich.edu>
2021-12-07 20:33:20 -0500

    Seperate HTML export template from screen file

--- BillSplitScreen.js
+++ BillSplitScreen.js
@@ -8,6 +8,7 @@ import { headerStyles, detailStyles, buttonStyles, rowStyles, containerStyles, l
 import { Ionicons, MaterialIcons, AntDesign  } from '@expo/vector-icons'; 
 import { getMemberModel, resetMemberModel } from './MemberModel';
 import { getItemModel, resetItemModel } from './ItemModel';
+import { getBillsExportHTML } from './exportTemplate';
 
 export default function BillSplitScreen({navigation, route}){
   const email = route.params.email;
@@ -17,64 +18,10 @@ export default function BillSplitScreen({navigation, route}){
   const [memberList, setMemberList] = useState([]);
   const [itemList, setItemList] = useState([]);
 
-  let summaryTableBody = "";
-  let detailTableBody = "";
-  for (member of memberList) {
-    let tr = `
-      <tr>
-        <td>${member.email} </td>
-        <td>${member.balance}</td>
-      </tr>
-    `;
-    summaryTableBody += tr;
-  }
-  for (item of itemList) {
-    let tr = `
-      <tr>
-        <td>${item.name} </td>
-        <td>${item.value}</td>
-        <td>${item.payer}</td>
-      </tr>
-    `;
-    detailTableBody += tr;
-  }
-
-  const html = `
-  <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-  </head>
-  <body style="text-align: center;">
-    <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
-      ${group.name} Bills
-    </h1>
-    <h2 style="font-size: 30px; font-family: Helvetica Neue; font-weight: normal;">
-      Balance Summary
-    </h2>
-    <Table style="text-align:center; font-size: 20px; font-family: Helvetica Neue; font-weight: normal; margin: 0 auto;">
-      <tr>
-        <th>Email</th>
-        <th>Balance</th>
-      </tr>
-      ${summaryTableBody}
-    </Table>         
-    <h2 style="font-size: 30px; font-family: Helvetica Neue; font-weight: normal;">Details</h2>
-    <Table style="text-align:center; font-size: 20px; font-family: Helvetica Neue; font-weight: normal; margin: 0 auto;">
-      <tr>
-        <th>Name</th>
-        <th>Value</th>
-        <th>Payer</th>
-      </tr>
-      ${detailTableBody}
-    </Table> 
-  </body>
-</html>
-  `;
-
   const printToFile = async () => {
     // On iOS/android prints the given html. On web prints the HTML from the current page.
     const { uri } = await Print.printToFileAsync({
-      html
+      html: getBillsExportHTML(group)
     });
     console.log('File has been saved to:', uri);
     await shareAsync(uri, { UTI: '.pdf', mimeType: 'application/pdf' });
---
+++ exportTemplate.js
@@ -0,0 +1,65 @@
+import { getMemberModel } from './MemberModel';
+import { getItemModel } from './ItemModel';
+
+export function getBillsExportHTML(group) {
+  const memberModel = getMemberModel(group);
+  const itemModel = getItemModel(group.groupId);
+  let memberList = memberModel.getMemberList();
+  let itemList = itemModel.itemList
+
+  let summaryTableBody = "";
+  let detailTableBody = "";
+  for (member of memberList) {
+    let tr = `
+      <tr>
+        <td>${member.email} </td>
+        <td>${member.balance}</td>
+      </tr>
+    `;
+    summaryTableBody += tr;
+  }
+  for (item of itemList) {
+    let tr = `
+      <tr>
+        <td>${item.name} </td>
+        <td>${item.value}</td>
+        <td>${item.payer}</td>
+      </tr>
+    `;
+    detailTableBody += tr;
+  }
+
+  const html = `
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+    </head>
+    <body style="text-align: center;">
+      <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
+        ${group.name} Bills
+      </h1>
+      <h2 style="font-size: 30px; font-family: Helvetica Neue; font-weight: normal;">
+        Balance Summary
+      </h2>
+      <Table style="text-align:center; font-size: 20px; font-family: Helvetica Neue; font-weight: normal; margin: 0 auto;">
+        <tr>
+          <th>Email</th>
+          <th>Balance</th>
+        </tr>
+        ${summaryTableBody}
+      </Table>         
+      <h2 style="font-size: 30px; font-family: Helvetica Neue; font-weight: normal;">Details</h2>
+      <Table style="text-align:center; font-size: 20px; font-family: Helvetica Neue; font-weight: normal; margin: 0 auto;">
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          <th>Payer</th>
+        </tr>
+        ${detailTableBody}
+      </Table> 
+    </body>
+  </html>
+  `;
+
+  return html;
+}
